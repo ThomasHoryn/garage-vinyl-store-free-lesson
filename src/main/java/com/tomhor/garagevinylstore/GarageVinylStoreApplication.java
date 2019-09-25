ed22328680b4dfbab0a6976562d1361ea7fe1e2f
Thomas Horyn <horyn.t@gmail.com>
2019-09-25 13:27:06 +0200

    Added custom .gitignore file.

---
+++ src/main/java/com/tomhor/garagevinylstore/GarageVinylStoreApplication.java
@@ -0,0 +1,13 @@
+package com.tomhor.garagevinylstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageVinylStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GarageVinylStoreApplication.class, args);
+    }
+
+}
